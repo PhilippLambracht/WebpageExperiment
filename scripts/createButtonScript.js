9e7778d00523b6d2b929b7a59d725bddfdd1dcf6
PhilippLambracht <plambracht@uni-koblenz.de>
2015-10-04 23:18:09 +0200

    style display test

--- scripts/createButtonScript.js
+++ scripts/createButtonScript.js
@@ -1,7 +1,7 @@
 
 document.getElementById("createButton").addEventListener("click", createFeatButtons);
-
-
+document.getElementById("unaliveIt").addEventListener("click", unaliveIt);
+document.getElementById("aliveIt").addEventListener("click", aliveIt);
 function AddBewButton (){
 alert('funtion triggert')
   var newButton = document.createElement("BUTTON");
@@ -15,11 +15,21 @@ function createFeatButtons (){
   //var lvls = parseFloat(document.getElementById('lvls').value);
   var lvlinput = parseFloat(document.getElementById('lvls').value);
   alert(lvlinput);
-  
+
 }
 
 function makeAButton (lvl){
   //var lvls = parseFloat(document.getElementById('lvls').value);
 
 
+}
+
+function unaliveIt(){
+  var target =  document.getElementById("happy");
+  target.style.display ="none";
+}
+
+function aliveIt(){
+  var target =  document.getElementById("happy");
+  target.style.display =""
 }
